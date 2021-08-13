8b7ea96fa04841a60b8bd9d3be1a68c48aea4e58
Yuliya.N <ynad2021@gmail.com>
2021-08-13 16:44:53 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+    const arr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen ', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    const arrDozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) {
+        result = 'zero';
+    }
+    else if (number < 20) {
+        result = `${arr[number]}`;
+    }
+    else if (number < 100) {
+        result = `${arrDozens[Math.floor(number / 10)]} ${arr[number % 10]}`;
+    }
+    else if (number >= 100 && number % 100 < 20) {
+        result = `${arr[Math.floor(number / 100)]} hundred ${arr[Math.floor(number % 100)]}`;
+    }
+    else if (number >= 100 && number % 100 >= 20) {
+        result = `${arr[Math.floor(number / 100)]} hundred ${arrDozens[Math.floor(number % 100 / 10)]} ${arr[number % 10]}`;
+    }
+    else {
+        result = `${arr[Math.floor(number / 100)]} hundred ${arr[number % 10]}`;
+    };
+    return result.trim();
+};
